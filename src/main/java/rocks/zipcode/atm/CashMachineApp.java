fc7f3cea44c66f4864ebdea7e487ea79d00015d1
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-22 15:37:17 -0500

    fixing git

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -1,4 +1,5 @@
 package rocks.zipcode.atm;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
@@ -13,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
+
 /**
  * @author ZipCodeWilmington
  */
@@ -21,54 +23,71 @@ public class CashMachineApp extends Application {
     private TextField field2 = new TextField();
     private CashMachine cashMachine = new CashMachine(new Bank());
     Scene scene1, scene2;
+
     @Override
     public void start(Stage primaryStage) {
+
         primaryStage.setTitle("REGS ATM");
+
 //Scene 1
         Label label1= new Label("Welcome!\n Please enter your account number.");
 //        Button button1= new Button("Go to scene 2");
 //        button1.setOnAction(e -> primaryStage.setScene(scene2));
         TextArea areaInfo = new TextArea();
+
         Button btnSubmit = new Button("Login");
         btnSubmit.setOnAction(e -> {
             int id = Integer.parseInt(field1.getText());
             cashMachine.login(id);
+
             areaInfo.setText(cashMachine.toString());
             primaryStage.setScene(scene2);
         });
+
         VBox layout1 = new VBox(10);
         layout1.getChildren().addAll(label1,field1 , btnSubmit, areaInfo);
         scene1= new Scene(layout1, 400, 400);
+
 //Scene 2
         Label label2= new Label("Please select an option.");
-        TextArea depositInfo = new TextArea();
+
+        TextArea withdrawDeposit = new TextArea();
         Button btnDeposit = new Button("Deposit");
         btnDeposit.setOnAction(e -> {
             int amount = Integer.parseInt(field2.getText());
             cashMachine.deposit(amount);
+
             areaInfo.setText(cashMachine.toString());
-            depositInfo.setText("Deposit Successful");
+            withdrawDeposit.setText("Deposit Successful");
+
         });
         Button btnWithdraw = new Button("Withdraw");
-        TextArea withdrawInfo = new TextArea();
+
         btnWithdraw.setOnAction(e -> {
             int amount = Integer.parseInt(field2.getText());
             cashMachine.withdraw(amount);
+
             areaInfo.setText(cashMachine.toString());
-            withdrawInfo.setText("Withdraw Successful");
+            withdrawDeposit.setText("Withdraw Successful");
         });
+
         Button btnExit = new Button("Exit");
         btnExit.setOnAction(e -> {
             cashMachine.exit();
             primaryStage.setScene(scene1);
             areaInfo.setText(cashMachine.toString());
         });
+
+
         VBox layout2= new VBox(20);
-        layout2.getChildren().addAll(label2, btnDeposit,btnWithdraw,btnExit,field2,areaInfo, depositInfo, withdrawInfo);
-        scene2= new Scene(layout2,400,600);
+        layout2.getChildren().addAll(label2, btnDeposit,btnWithdraw,btnExit,field2,areaInfo, withdrawDeposit);
+        scene2= new Scene(layout2,400,400);
+
+
         primaryStage.setScene(scene1);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
